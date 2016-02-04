d132207ca7d8e07fd6ca159fd8e2dbafd754fda9
Byteandahalf <ryanterry.byte@gmail.com>
2016-02-04 00:30:55 -0500

    We're getting so close, still doesn't properly inject code
    But it doesn't crash for me.
    
    Credits to
    @byteandahalf,
    @jocopa3,
    @C0deh4cker

--- codeinject.cpp
+++ codeinject.cpp
@@ -1,80 +1,91 @@
 #include "stdafx.h"
-#include <iostream>
 #include <Windows.h>
-#include <TlHelp32.h>
-using namespace std;
 
-DWORD_PTR GetProcessBaseAddress(DWORD processID)
-{
-    DWORD_PTR   baseAddress = 0x00000000;
-    HANDLE      processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
-    HMODULE     *moduleArray;
-    LPBYTE      moduleArrayBytes;
-    DWORD       bytesRequired;
+// base address; TODO: Find this dynamically
+static uintptr_t* const BaseAddress = (uintptr_t*)0x7ff6b24c0000;
 
-    if(processHandle)
-    {
-        if(EnumProcessModules(processHandle, NULL, 0, &bytesRequired))
-        {
-            if(bytesRequired)
-            {
-                moduleArrayBytes = (LPBYTE) LocalAlloc(LPTR, bytesRequired);
 
-                if(moduleArrayBytes)
-                {
-                    unsigned int moduleCount;
-
-                    moduleCount = bytesRequired / sizeof(HMODULE);
-                    moduleArray = (HMODULE*) moduleArrayBytes;
-
-                    if(EnumProcessModules(processHandle, moduleArray, bytesRequired, &bytesRequired))
-                    {
-                        baseAddress = (DWORD_PTR) moduleArray[0];
-                    }
-
-                    LocalFree(moduleArrayBytes);
-                }
-            }
-        }
-
-        CloseHandle(processHandle);
-    }
-
-    return baseAddress;
+uintptr_t* slideAddress(uintptr_t offset) {
+	return (uintptr_t*)((uintptr_t)BaseAddress + offset);
 }
 
-int main()
+bool shouldRenderBlock()
 {
-	/* this base address is nothing so far */
-    DWORD_PTR address = 0x00000000;
-    HWND hwnd = FindWindow(0, L"Minecraft: Windows 10 Edition Beta");
-    DWORD pid;
-    int data = 0;
-    int newData = 0;
+	return false;
+}
 
-    if(hwnd)
-    {
-        GetWindowThreadProcessId(hwnd, &pid);
-        HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
-        if(phandle)
-        {
-            DWORD_PTR base = GetProcessBaseAddress(pid);
-            cout << "Base address located: " << (uintptr_t) base << endl;
+bool minecraftH4x0r() {
 
-            ReadProcessMemory(phandle, (LPCVOID)(base + address), &data, sizeof(data), 0);
-        }
-        else
-        {
-            cout << "Handle not found" << endl;
-        }
-    }
-    else
-    {
-        cout << "Couldn't find window" << endl;
-    }
+	uintptr_t* const vtable_address = slideAddress(0x9965E8);
+	// ptr to _ZTV5Block
+	uintptr_t** const vtable = (uintptr_t** const)vtable_address;
 
-    cin.get();
+	DWORD procId;
+	HWND hWnd = FindWindow(0, L"Minecraft.Win10.DX11.exe");
+	if (hWnd == NULL)
+	{
+		return false;
+	}
 
-    cout << "Code successfully injected!" << endl;
-    return 0;
+	GetWindowThreadProcessId(hWnd, &procId);
+	HANDLE process = OpenProcess(PROCESS_ALL_ACCESS | PROCESS_VM_OPERATION | PROCESS_QUERY_INFORMATION, FALSE, procId);
+	
+	// Attempt to allow SE_DEBUG privileges to this process
+	HANDLE hToken;
+	TOKEN_PRIVILEGES _privileges;
+	OpenProcessToken(process, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
+	LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &_privileges.Privileges[0].Luid);
+
+	_privileges.PrivilegeCount = 1;
+	_privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+	AdjustTokenPrivileges(hToken, FALSE, &_privileges, 0, (PTOKEN_PRIVILEGES)NULL, 0);
+
+	if (GetLastError() != ERROR_SUCCESS)
+	{
+		CloseHandle(process);
+		return false;
+	}
+
+	// Store old and new protections
+	DWORD protection = PAGE_READWRITE;
+	DWORD oldProtection;
+
+	// Index of Block::shouldRenderFace in the vtable
+	int index = 6;
+
+	// Attempt to set read/write protections
+	if (::VirtualProtectEx(process, &vtable[index], sizeof(DWORD_PTR), protection, &oldProtection) == 0) {
+		CloseHandle(process);
+		return false;
+	}
+	else // Replace the vtable pointer for Block::shouldRenderFace
+		vtable[index] = (uintptr_t*) &shouldRenderBlock;
+
+	// Revert back to previous permissions
+	if (::VirtualProtectEx(process, &vtable[index], sizeof(DWORD_PTR), oldProtection, &oldProtection) == 0) {
+		CloseHandle(process);
+		return false;
+	}
+
+	CloseHandle(process);
+	return true;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		if (!minecraftH4x0r())
+		{
+			; // TODO: Popup that we failed
+		}
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
